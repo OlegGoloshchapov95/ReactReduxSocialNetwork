901b18be6429429edaf496b24019871c1769475e
OlegGoloshchapov95 <oleggol95@gmail.com>
2021-06-23 12:55:47 +0300

    React && Redux build

---
+++ build/precache-manifest.f374a59bf74fd62fe25b4302e82fc77e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a1849d420b9b294e8cc4804b8d4703",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7558163878bd46aa870c",
+    "url": "/static/css/main.da518b5d.chunk.css"
+  },
+  {
+    "revision": "acf1264c274745c97fcb",
+    "url": "/static/js/2.2a3af1f3.chunk.js"
+  },
+  {
+    "revision": "7558163878bd46aa870c",
+    "url": "/static/js/main.2a179a2a.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  },
+  {
+    "revision": "fc93ce742a7343d9305d18987e0221ca",
+    "url": "/static/media/cat.fc93ce74.jpg"
+  },
+  {
+    "revision": "e803ebaaf3fc6158d7abc082942d79ac",
+    "url": "/static/media/cat2.e803ebaa.jpg"
+  },
+  {
+    "revision": "602e2b5874a12ba30886624324164ddc",
+    "url": "/static/media/main-logo.602e2b58.png"
+  }
+]);
